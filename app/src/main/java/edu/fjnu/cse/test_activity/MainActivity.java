5db7448ff46c26cbdf3bd9ac06bf4968aa861bfd
panwenxia <978619817@qq.com>
2018-03-25 17:34:27 +0800

    Initial commit

---
+++ app/src/main/java/edu/fjnu/cse/test_activity/MainActivity.java
@@ -0,0 +1,13 @@
+package edu.fjnu.cse.test_activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
